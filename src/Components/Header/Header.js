e703bae667a9a2befaaace5318d90110edb241b9
azer <benazzouzaze@gmail.com>
2022-11-24 22:18:38 +0100

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <React.Fragment>
+        <div style={{"height":"20rem"}} className="bg-primary">
+            
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Header
