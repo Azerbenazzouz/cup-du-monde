7c497d36c7e8f78d197363378eedc3ee1dd689c5
azer <benazzouzaze@gmail.com>
2022-11-24 23:04:54 +0100

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Header.css";
 import logo from "./asset/logo.png"
+import qatar_word from "./asset/qatar-word.png"
+
 function Header() {
   return (
     <React.Fragment>
@@ -9,8 +11,18 @@ function Header() {
         </div>
         <div id="header" style={{"height":"20rem"}} className="bg-primary">
             <div id="header-background">
-
+                <div id="header-title" className="p-5">
+                    <h1 >
+                        كأس
+                    </h1>
+                    <h1 >
+                        العالم
+                    </h1>
+                </div>
             </div>
+        </div>
+        <div className="d-flex justify-content-center" style={{"marginTop":"30px"}}>
+            <img src={qatar_word} alt="qatar word" style={{"width":"400px"}}/>
         </div>
     </React.Fragment>
   )
