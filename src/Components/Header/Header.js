7d0fbb9b42d8649fb3fe2faa5a89537eb62ed480
azer <benazzouzaze@gmail.com>
2022-11-24 22:46:59 +0100

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import "./Header.css";
+import logo from "./asset/logo.png"
 function Header() {
   return (
     <React.Fragment>
-        <div style={{"height":"20rem"}} className="bg-primary">
-            
+        <div id="logo-header">
+            <img src={logo} alt="logo" id="logo-img" />
+        </div>
+        <div id="header" style={{"height":"20rem"}} className="bg-primary">
+            <div id="header-background">
+
+            </div>
         </div>
     </React.Fragment>
   )
